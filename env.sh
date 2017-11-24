e2de6017e0687368b9d4c298fc74dcec81dbeb1e
Tomas Jansson <tomas.jansson@gmail.com>
2017-11-24 12:27:31 +0100

    global gitignore, python bin in path

--- env.sh
+++ env.sh
@@ -2,8 +2,9 @@
 
 # Exports
 
-export PATH=$PATH:/Library/Frameworks/Mono.framework/Versions/Current/bin/:~/git/unacast/scrappy-scripts/
 export GOPATH=$HOME/golang
+export PATH=$PATH:/Library/Frameworks/Mono.framework/Versions/Current/bin/:~/git/unacast/scrappy-scripts/src:$GOPATH/bin
+export PATH=$PATH:~/Library/Python/2.7/bin
 
 # Functions
 function mkcd() { mkdir -p "$@" && cd "$_"; }
