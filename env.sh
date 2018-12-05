177f0201f8845b68b725b7077bc57577f2314efc
Tomas Jansson <tomas.jansson@gmail.com>
2018-12-05 14:55:15 +0100

    Add dotnet to path

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@
 # Exports
 
 export GOPATH=$HOME/golang
-export PATH=$PATH:/Library/Frameworks/Mono.framework/Versions/Current/bin/:~/git/unacast/scrappy-scripts/src:$GOPATH/bin
+export PATH=$PATH:/Library/Frameworks/Mono.framework/Versions/Current/bin/:~/git/unacast/scrappy-scripts/src:$GOPATH/bin:~/.dotnet/tools
 export PATH=$PATH:~/Library/Python/2.7/bin
 
 # Functions
